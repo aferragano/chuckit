a2d5f456e98c331979f29af6ac669885fbf12bbf
AnthonyF <aferragano@gmail.com>
2015-12-02 15:26:57 -0500

    able to bring in more data from ase to dispay on dom. styl sales field. sale field needs logic in order to sift out non sale. need to add  regular price need to add sale price need to add search. need to add display page need to add login capabilities need to add shopping cart need to ad display shopping cart. both modal and page. show previous history. etc

--- server/starup/loadProducts.js
+++ server/starup/loadProducts.js
@@ -4,6 +4,68 @@ Meteor.startup(function() {
 			{ 	
 				'itemNumber': 53245,
 				'productTags': [ "dog toy", "ball" ],
+				'name': 'Chuckit2! Indoor Ball',
+				'brand': 'Petmate',
+				'price': '4.99',
+				'sale': "true",
+				'imageUrl': "http://ecx.images-amazon.com/images/I/51lXNf7piFL.jpg",			
+				'stock': '20',
+				'description': "No yelling at Fido for bringing this ball in the house...that's exactly where it belongs! The Chuckit! Indoor Ball is the perfect compliment to rainy days, hardwood floors, or late-night games of fetch. The lightweight Bounceflex Core Technology makes it soft enough when meeting a wall or furniture while giving it just the right amount of bounce for indoor play. Use it with the Indoor Launcher for slobber-free fun.",
+				'reviews': [
+					{
+							"stars": 4,
+							"title": "best toy ever",
+							"date": "date",
+							"reviewContent": " review content",
+							"userName": "Bonnie74",
+							"location": "Long Island",
+							"reccomend": true,
+							"helpfull":  3
+					},
+					{
+							"stars": 4,
+							"title": "best toy ever",
+							"date": "date",
+							"reviewContent": " review content",
+							"userName": "Bonnie74",
+							"location": "Long Island",
+							"helpfull":  3
+					}
+				]
+			},
+			{ 	
+				'itemNumber': 53245,
+				'productTags': [ "dog toy", "ball" ],
+				'name': 'Chuckit! Indoor Ball',
+				'brand': 'Petmate',
+				'price': '4.99',
+				'stock': '20',
+				'description': "No yelling at Fido for bringing this ball in the house...that's exactly where it belongs! The Chuckit! Indoor Ball is the perfect compliment to rainy days, hardwood floors, or late-night games of fetch. The lightweight Bounceflex Core Technology makes it soft enough when meeting a wall or furniture while giving it just the right amount of bounce for indoor play. Use it with the Indoor Launcher for slobber-free fun.",
+				'reviews': [
+					{
+							"stars": 4,
+							"title": "best toy ever",
+							"date": "date",
+							"reviewContent": " review content",
+							"userName": "Bonnie74",
+							"location": "Long Island",
+							"reccomend": true,
+							"helpfull":  3
+					},
+					{
+							"stars": 4,
+							"title": "best toy ever",
+							"date": "date",
+							"reviewContent": " review content",
+							"userName": "Bonnie74",
+							"location": "Long Island",
+							"helpfull":  3
+					}
+				]
+			},
+			{ 	
+				'itemNumber': 53245,
+				'productTags': [ "dog toy", "ball" ],
 				'name': 'Chuckit! Indoor Ball',
 				'brand': 'Petmate',
 				'price': '4.99',
@@ -34,6 +96,16 @@ Meteor.startup(function() {
 		];
 
 		for (var i = 0; i < products.length; i++)
-			Products.insert({name: products[i].name, price: products[i].price});
+			Products.insert({
+				itemNumber: products[i].itemNumber,
+				productTags: products[i].productTags,
+				brand: products[i].brand, 
+				sale: products[i].sale,
+				imageUrl: products[i].imageUrl,
+				description: products[i].description,
+				reviews: products[i].reviews,
+				stock: products[i].stock,
+				name: products[i].name, 
+				price: products[i].price});
 	}
 })
