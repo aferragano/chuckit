6c0cd46593be4158e6bad611e65407853fe34b75
AnthonyF <aferragano@gmail.com>
2015-12-06 16:10:47 -0500

    product scoll . product list on mobile.

--- server/starup/loadProducts.js
+++ server/starup/loadProducts.js
@@ -41,6 +41,117 @@ Meteor.startup(function() {
 			{ 	
 				'itemNumber': 53245,
 				'productTags': [ "dog toy", "ball" ],
+				'name': 'Chuckit2! Indoor Ball',
+				'brand': 'Petmate',
+				'price': '4.9',
+				'sale': "true",
+				'images': [
+					{ full: "http://ecx.images-amazon.com/images/I/51lXNf7piFL.jpg"},
+					{ full: "http://ecx.images-amazon.com/images/I/81Th9AO4cGL._SL1500_.jpg"},
+					{ full: "http://ecx.images-amazon.com/images/I/71IceeHLGoL._SL1500_.jpg"},
+					{ full: "http://ecx.images-amazon.com/images/I/31kCHXiN3nL.jpg"},
+						],			
+				'stock': '20',
+				'description': "No yelling at Fido for bringing this ball in the house...that's exactly where it belongs! The Chuckit! Indoor Ball is the perfect compliment to rainy days, hardwood floors, or late-night games of fetch. The lightweight Bounceflex Core Technology makes it soft enough when meeting a wall or furniture while giving it just the right amount of bounce for indoor play. Use it with the Indoor Launcher for slobber-free fun.",
+				'reviews': [
+					{
+							"stars": 4,
+							"title": "best toy ever",
+							"date": "",
+							"content": "content ten co content tent co co co coten aplsbid apo",
+							"userName": "Bonnie74",
+							"location": "Long Island",
+							"reccomend": true,
+							"helpfull":  3
+					},
+					{
+							"stars": 4,
+							"title": "best toy ever",
+							"date": "",
+							"content": "content ten co content tent co co co coten aplsbid apo",
+							"userName": "Bonnie74",
+							"location": "Long Island",
+							"helpfull":  3
+					}
+				]
+			},
+			{ 	
+				'itemNumber': 53245,
+				'productTags': [ "dog toy", "ball" ],
+				'name': 'Chuckit2! Indoor Ball',
+				'brand': 'Petmate',
+				'price': '4.9',
+				'sale': "true",
+				'images': [
+					{ full: "http://ecx.images-amazon.com/images/I/51lXNf7piFL.jpg"},
+					{ full: "http://ecx.images-amazon.com/images/I/81Th9AO4cGL._SL1500_.jpg"},
+					{ full: "http://ecx.images-amazon.com/images/I/71IceeHLGoL._SL1500_.jpg"},
+					{ full: "http://ecx.images-amazon.com/images/I/31kCHXiN3nL.jpg"},
+						],			
+				'stock': '20',
+				'description': "No yelling at Fido for bringing this ball in the house...that's exactly where it belongs! The Chuckit! Indoor Ball is the perfect compliment to rainy days, hardwood floors, or late-night games of fetch. The lightweight Bounceflex Core Technology makes it soft enough when meeting a wall or furniture while giving it just the right amount of bounce for indoor play. Use it with the Indoor Launcher for slobber-free fun.",
+				'reviews': [
+					{
+							"stars": 4,
+							"title": "best toy ever",
+							"date": "",
+							"content": "content ten co content tent co co co coten aplsbid apo",
+							"userName": "Bonnie74",
+							"location": "Long Island",
+							"reccomend": true,
+							"helpfull":  3
+					},
+					{
+							"stars": 4,
+							"title": "best toy ever",
+							"date": "",
+							"content": "content ten co content tent co co co coten aplsbid apo",
+							"userName": "Bonnie74",
+							"location": "Long Island",
+							"helpfull":  3
+					}
+				]
+			},
+			{ 	
+				'itemNumber': 53245,
+				'productTags': [ "dog toy", "ball" ],
+				'name': 'Chuckit2! Indoor Ball',
+				'brand': 'Petmate',
+				'price': '4.9',
+				'sale': "true",
+				'images': [
+					{ full: "http://ecx.images-amazon.com/images/I/51lXNf7piFL.jpg"},
+					{ full: "http://ecx.images-amazon.com/images/I/81Th9AO4cGL._SL1500_.jpg"},
+					{ full: "http://ecx.images-amazon.com/images/I/71IceeHLGoL._SL1500_.jpg"},
+					{ full: "http://ecx.images-amazon.com/images/I/31kCHXiN3nL.jpg"},
+						],			
+				'stock': '20',
+				'description': "No yelling at Fido for bringing this ball in the house...that's exactly where it belongs! The Chuckit! Indoor Ball is the perfect compliment to rainy days, hardwood floors, or late-night games of fetch. The lightweight Bounceflex Core Technology makes it soft enough when meeting a wall or furniture while giving it just the right amount of bounce for indoor play. Use it with the Indoor Launcher for slobber-free fun.",
+				'reviews': [
+					{
+							"stars": 4,
+							"title": "best toy ever",
+							"date": "",
+							"content": "content ten co content tent co co co coten aplsbid apo",
+							"userName": "Bonnie74",
+							"location": "Long Island",
+							"reccomend": true,
+							"helpfull":  3
+					},
+					{
+							"stars": 4,
+							"title": "best toy ever",
+							"date": "",
+							"content": "content ten co content tent co co co coten aplsbid apo",
+							"userName": "Bonnie74",
+							"location": "Long Island",
+							"helpfull":  3
+					}
+				]
+			},
+			{ 	
+				'itemNumber': 53245,
+				'productTags': [ "dog toy", "ball" ],
 				'name': 'Chuckit! Indoor Ball',
 				'brand': 'Petmate',
 				'price': '4.99',
